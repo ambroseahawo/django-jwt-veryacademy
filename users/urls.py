ce366b4db3e6d72ddaddabb4b149346cb61e6e35
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-14 01:09:14 +0300

    Implement users urls

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from users.views import CustomUserCreate, BlacklistTokenUpdateView
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', CustomUserCreate.as_view(), name='create_user'),
+    path('register/', CustomUserCreate.as_view(), name='register_user'),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist')
 ]
