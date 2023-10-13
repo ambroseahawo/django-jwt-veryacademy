f3975cabaa5cef760e8ec14967acccf42c8fd2fb
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-14 01:04:17 +0300

    Implement blacklist token apiview

--- users/views.py
+++ users/views.py
@@ -19,3 +19,17 @@ class CustomUserCreate(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class BlacklistTokenUpdateView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = ()
+    
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
